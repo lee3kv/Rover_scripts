f0886fcab6135fcb9bb4e628075184298af100d9
lee3kv <lee3kv@mail.uc.edu>
2022-04-10 11:51:49 -0400

    added camera files

--- python/camera.py
+++ python/camera.py
@@ -2,11 +2,13 @@ import cv2
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0)
+        self.dispW = 960
+        self.dispH = 720
+        self.video = cv2.VideoCapture('nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=3 ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink')
 
     def __del__(self):
         self.video.release()
-        
+
     def get_frame(self):
         ret, frame = self.video.read()
 
