c2d3ba0f9d0cebc25bacb24aa1537c4d0d01bf18
lee3kv <lee3kv@mail.uc.edu>
2022-04-11 02:24:50 -0400

    minor changes to camera files***

--- python/camera.py
+++ python/camera.py
@@ -4,7 +4,7 @@ class VideoCamera(object):
     def __init__(self):
         self.dispW = 960
         self.dispH = 720
-        self.video = cv2.VideoCapture('nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=3 ! video/x-raw, width='+str(self.dispW)+', height='+str(self.dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink')
+        self.video = cv2.VideoCapture('nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=3 ! video/x-raw, width='+str(self.dispW)+', height='+str(self.dispH)+', format=I420 ! videoconvert ! video/x-raw, format=BGR ! appsink')
 
     def __del__(self):
         self.video.releast()
