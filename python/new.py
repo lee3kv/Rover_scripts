fdbc237e6a208b7f93ef238faf0c3510d8f29cdd
lee3kv <36865187+lee3kv@users.noreply.github.com>
2022-04-11 15:55:34 -0400

    Update new.py

--- python/new.py
+++ python/new.py
@@ -1,5 +1,7 @@
 import cv2
-
+dispW=320
+dispH=240
+flip=3
 camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
 cam = cv2.VideoCapture(camSet)
 while True:
