c64cbf4b824283310219efa183bc31fc0105dfdf
lee3kv <36865187+lee3kv@users.noreply.github.com>
2022-04-11 18:49:06 -0400

    Update another.py

--- python/another.py
+++ python/another.py
@@ -3,8 +3,17 @@ import gpsd
 # Connect to the local gpsd
 gpsd.connect()
 
-# Get gps position
-packet = gpsd.get_current()
-
+def get_location():
+  # Get gps position
+  packet = gpsd.get_current()
+  
+  try:
+    lat, lon = packet.position()
+    return lat, lon
+  except gpsd.NoFixError:
+    return -999.99, -999.99
+  
 # See the inline docs for GpsResponse for the available data
-print(packet.position())
+a,b = get_location
+print(a)
+print(b)
