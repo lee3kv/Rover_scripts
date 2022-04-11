5204387a9e8da6b3c5610ffd2f14af5a1db0979b
lee3kv <36865187+lee3kv@users.noreply.github.com>
2022-04-11 19:10:58 -0400

    Update gps.py

--- python/gps.py
+++ python/gps.py
@@ -33,6 +33,26 @@ def data_format():
         "pressure": "{:.2f}".format(bme280.pressure),
         "altitude": "{:.2f}".format(bme280.altitude),
     }
+    
+ import gpsd
+
+# Connect to the local gpsd
+gpsd.connect()
+
+def get_location():
+  # Get gps position
+  packet = gpsd.get_current()
+  
+  try:
+    lat, lon = packet.position()
+    return lat, lon
+  except gpsd.NoFixError:
+    return -999.99, -999.99
+  
+# See the inline docs for GpsResponse for the available data
+a,b = get_location()
+print(a)
+print(b)
 
     # updates data with the latest values
     update = {'$push': bme280data}
